8a0e8c06c6bdb1667d7f1464ff77e6daeafbb3f7
Suhaib <suhaibsworkspace@gmail.com>
2023-08-28 22:11:33 +0530

    Layout Adjustments, DEC to HEX Conversion, Code Editor Zoom_in Zoom_out

---
+++ controllers/conversion.js
@@ -0,0 +1,14 @@
+const decimal = document.getElementById("decimal");
+const hex = document.getElementById("hex");
+
+function decimalToHex() {
+    hex.value = parseInt(decimal.value).toString(16).toUpperCase();
+}
+
+function hexToDecimal() {
+    decimal.value = parseInt(hex.value, 16);
+    hex.value = hex.value.toUpperCase();
+}
+
+
+
